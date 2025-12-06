5dd11350810171c3e80d7fd0a5c817e9c04ba7e2
Elliot Yates <elliotyatesj@gmail.com>
2025-12-06 23:31:50 +0000

    Zip instead of numpy

--- 2025/day_06/day_06.py
+++ 2025/day_06/day_06.py
@@ -2,25 +2,21 @@ part1 = 0
 part2 = 0
 
 # Parse the input file, line by line
-input = [line.strip().split() for line in open("day_06_input.txt", 'r')]
+input = [list(line.strip().split()) for line in open("day_06_test.txt", 'r')]
 
-import numpy as np
 import math
 
-# Process all integers (excluding symbols) into numpy array
-input_np = np.asarray(input[:-1], dtype='int64')
-
-# Transpose the numpy
-input_np = np.transpose(input_np)
+# Transpose from rows to columns with zip
+nums_transposed = list(zip(*input[:-1]))
 
 # Loop through the symbols
 for idx, symbol in enumerate(input[-1]):
     # If symbol '+', perform sum
     if symbol == "+":
-        part1 += sum(list(input_np[idx]))
+        part1 += sum(map(int, nums_transposed[idx]))
     # If symbol '*', multiply out
     if symbol == "*":
-        part1 += math.prod(list(input_np[idx]))
+        part1 += math.prod(map(int, nums_transposed[idx]))
 
 # Part 2 needs to be completely recalculated, as stripped whitespace in part 1
 
