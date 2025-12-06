4449391277bebe051d8f2b032dda4a8472d987bb
Elliot Yates <elliotyatesj@gmail.com>
2025-12-06 22:22:25 +0000

    Making some progess, not there yet

--- 2025/day_06/day_06.py
+++ 2025/day_06/day_06.py
@@ -2,7 +2,7 @@ part1 = 0
 part2 = 0
 
 # Parse the input file, line by line
-input = [line.strip().split() for line in open("day_06_input.txt", 'r')]
+input = [line.strip().split() for line in open("day_06_test.txt", 'r')]
 
 import numpy as np
 import operator
@@ -24,6 +24,43 @@ for idx, symbol in enumerate(input[-1]):
         for n in list(input_np[idx]):
             multiply *= n
         part1 += multiply
+
+from collections import defaultdict
+
+# Loop through the symbols
+for idx, symbol in enumerate(input[-1]):
+    # Extract total number of digits of the largest number
+    max_len = len(str(max(list(input_np[idx]))))
+
+    padded_nums = [str(num).rjust(max_len) for num in list(input_np[idx])]
+
+    print(padded_nums)
+
+    # position_by_index = defaultdict(list)
+
+    # for i in range(max_len - 1, -1, -1):
+    #     for n in list(input_np[idx]):
+    #         n = str(n)[::-1]
+    #         if i < len(n):
+    #             position_by_index[i].append(n[i])
+
+    # s = sorted(position_by_index.items())
+
+    # sum_nums = []
+
+    # for k,v in s:
+    #     merged_num = "".join(v)
+    #     sum_nums.append(int(merged_num))
+
+    # # If symbol '+', perform sum
+    # if symbol == "+":
+    #     part2 += sum(list(sum_nums))
+    # multiply = 1
+    # # If symbol '*', multiply out
+    # if symbol == "*":
+    #     for n in list(sum_nums):
+    #         multiply *= n
+    #     part2 += multiply
 
 print("Part 1:", part1)
 print("Part 2:", part2)
